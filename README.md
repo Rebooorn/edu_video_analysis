2455bd42d74f93ca85e9444e1bc05ecc727f6532
Chang Liu <chang.ch.liu@fau.de>
2024-05-31 12:31:47 +0200

    update readme

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
 # Facial and hand detection
 
-Here we will detect the hand/face key-points in a video:  
-1. [] Disassemble the .mp4 video to output each frame.
-2. [] Use mediapipe to detect keypoints in a frame.
-3. [] Detect keypoints in one video, and save the detection results.
-4. [] Process all videos.
+In this project, we will design a pipeline to:  
+1. detect the face/hand keypoints in each frame of a video.
+2. analyse the expression change based on the keypoints.
+3. research the correlation of the expression changes and the 
 
 ## Usage 
-1. install the requirements
+1. install the requirements  
+```console
+pip install -r requirements
+```
+
+2. Extract the holistic keypoints from the video by mediapipe.  
+```console
+python mp_utils.py -vf ./videos/xx.mp4 -of ./videos_outputs
+```
+This will output the xxx_body.csv, xxx_face.csv and xxx_hand.csv, recording the time-series keypoints of the 
+
+3. Analyse the head and facial expression in the detected time-series.  
+```console
+python detect_utils.py 
+```
--- mp_utils.py
+++ mp_utils.py
@@ -96,12 +96,11 @@ def cli_debug(args):
 
 
 def cli_mp_main(args):
-    mypath =  args.mypath    #this is your folder with (all) your video(s)
-    vfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))] #loop through the filenames and collect them in a list
+    # mypath =  args.mypath    #this is your folder with (all) your video(s)
+    vfile = args.vfile
+    # vfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))] #loop through the filenames and collect them in a list
     #time series output folder
-    inputfol = mypath
-    outputf_mask = args.outputf_ts
-    outputf_ts = outputf_mask
+    outputf_ts = args.outputf_ts
     
     verbose = args.verbose
     save_video = args.save_video
@@ -135,62 +134,62 @@ def cli_mp_main(args):
     masking = False
 
     #We will now loop over all the videos that are present in the video file
-    for vidf in vfiles:
-        print("We will now process video:")
-        print(vidf)
-        print("This is video number" + str(vfiles.index(vidf))+ "of" + str(len(vfiles)) + "videos in total")
-        #capture the video, and check video settings
-        videoname = vidf
-        videoloc = inputfol + videoname
-        videoloc = str(Path(inputfol) / videoname)
-        capture = cv2.VideoCapture(videoloc) #load in the videocapture
-        frameWidth = capture.get(cv2.CAP_PROP_FRAME_WIDTH) #check frame width
-        frameHeight = capture.get(cv2.CAP_PROP_FRAME_HEIGHT) #check frame height
-        samplerate = capture.get(cv2.CAP_PROP_FPS)   #fps = frames per second
+    # for vidf in vfiles:
+    print("We will now process video:")
+    print(vfile)
+    # print("This is video number" + str(vfiles.index(vidf))+ "of" + str(len(vfiles)) + "videos in total")
+    #capture the video, and check video settings
+    videoname = Path(vfile).name
+    videoloc = vfile
+    capture = cv2.VideoCapture(videoloc) #load in the videocapture
+    frameWidth = capture.get(cv2.CAP_PROP_FRAME_WIDTH) #check frame width
+    frameHeight = capture.get(cv2.CAP_PROP_FRAME_HEIGHT) #check frame height
+    samplerate = capture.get(cv2.CAP_PROP_FPS)   #fps = frames per second
 
-        #make an 'empty' video file where we project the pose tracking on
-        fourcc = cv2.VideoWriter_fourcc(*'MP4V') #for different video formats you could use e.g., *'XVID'
-        outputloc = str(Path(outputf_mask) / videoname)
-        if save_video:
-            out = cv2.VideoWriter(outputloc, fourcc, 
-                                fps = samplerate, frameSize = (int(frameWidth), int(frameHeight)))
-        # print(f'video info: fps={samplerate}, framesize=[{frameHeight}, {frameWidth}]')
-        # break
-        # Run MediaPipe frame by frame using Holistic with `enable_segmentation=True` to get pose segmentation.
-        time = 0
-        tsbody = [markerxyzbody]   #these will be your time series objects, which start with collumn names initialized above
-        tshands = [markerxyzhands] #these will be your time series objects, which start with collumn names initialized above
-        tsface = [markerxyzface]   #these will be your time series objects, which start with collumn names initialized above
-        with mp_holistic.Holistic(
-                static_image_mode=False, enable_segmentation=True, refine_face_landmarks=True) as holistic:
-            nframe=0
-            while (True):
-                ret, image = capture.read() #read frame
-                if ret == True: #if there is a frame
-                    image_ = resize(image, output_shape=(180, 270)) * 255
-                    cv2.imwrite(str(Path(outputf_mask)/'frames'/'{}.jpg'.format(nframe)), image_.astype(np.uint8))
-                    nframe+=1
-                    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) #make sure the image is in RGB format
-                    results = holistic.process(image) #apply Mediapipe holistic processing
-                    # Draw pose segmentation
-                    h, w, c = image.shape
-                    if  np.all(results.segmentation_mask) != None: #check if there is a pose found
-                        if masking == False:
-                            original_image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-                        if masking == True:
-                            original_image = np.concatenate([image, np.full((h, w, 1), 255, dtype=np.uint8)], axis=-1)
-                            mask_img = np.zeros_like(image, dtype=np.uint8) #set up basic mask image
-                            mask_img[:, :] = (255,255,255) #set up basic mask image
-                            segm_2class = 0.2 + 0.8 * results.segmentation_mask #set up a segmentation of the results of mediapipe
-                            segm_2class = np.repeat(segm_2class[..., np.newaxis], 3, axis=2) #set up a segmentation of the results of mediapipe
-                            annotated_image = mask_img * segm_2class * (1 - segm_2class) #take the basic mask image and make a sillhouette mask
-                            # append Alpha channel to sillhouetted mask so that we can overlay it to the original image
-                            mask = np.concatenate([annotated_image, np.full((h, w, 1), 255, dtype=np.uint8)], axis=-1)
-                            # Zero background where we want to overlay
-                            original_image[mask==0]=0 #for the original image we are going to set everything at zero for places where the mask has to go
-                            original_image = cv2.cvtColor(original_image, cv2.COLOR_RGB2BGR)
-                        #now lets draw on the original_image the left and right hand landmarks, the facemesh and the body poses
-                            #left hand
+    #make an 'empty' video file where we project the pose tracking on
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V') #for different video formats you could use e.g., *'XVID'
+    outputloc = str(Path(outputf_ts) / videoname)
+    if save_video:
+        out = cv2.VideoWriter(outputloc, fourcc, 
+                            fps = samplerate, frameSize = (int(frameWidth), int(frameHeight)))
+    # print(f'video info: fps={samplerate}, framesize=[{frameHeight}, {frameWidth}]')
+    # break
+    # Run MediaPipe frame by frame using Holistic with `enable_segmentation=True` to get pose segmentation.
+    time = 0
+    tsbody = [markerxyzbody]   #these will be your time series objects, which start with collumn names initialized above
+    tshands = [markerxyzhands] #these will be your time series objects, which start with collumn names initialized above
+    tsface = [markerxyzface]   #these will be your time series objects, which start with collumn names initialized above
+    with mp_holistic.Holistic(
+            static_image_mode=False, enable_segmentation=True, refine_face_landmarks=True) as holistic:
+        nframe=0
+        while (True):
+            ret, image = capture.read() #read frame
+            if ret == True: #if there is a frame
+                image_ = resize(image, output_shape=(180, 270)) * 255
+                # cv2.imwrite(str(Path(outputf_mask)/'frames'/'{}.jpg'.format(nframe)), image_.astype(np.uint8))
+                nframe+=1
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) #make sure the image is in RGB format
+                results = holistic.process(image) #apply Mediapipe holistic processing
+                # Draw pose segmentation
+                h, w, c = image.shape
+                if  np.all(results.segmentation_mask) != None: #check if there is a pose found
+                    if masking == False:
+                        original_image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                    if masking == True:
+                        original_image = np.concatenate([image, np.full((h, w, 1), 255, dtype=np.uint8)], axis=-1)
+                        mask_img = np.zeros_like(image, dtype=np.uint8) #set up basic mask image
+                        mask_img[:, :] = (255,255,255) #set up basic mask image
+                        segm_2class = 0.2 + 0.8 * results.segmentation_mask #set up a segmentation of the results of mediapipe
+                        segm_2class = np.repeat(segm_2class[..., np.newaxis], 3, axis=2) #set up a segmentation of the results of mediapipe
+                        annotated_image = mask_img * segm_2class * (1 - segm_2class) #take the basic mask image and make a sillhouette mask
+                        # append Alpha channel to sillhouetted mask so that we can overlay it to the original image
+                        mask = np.concatenate([annotated_image, np.full((h, w, 1), 255, dtype=np.uint8)], axis=-1)
+                        # Zero background where we want to overlay
+                        original_image[mask==0]=0 #for the original image we are going to set everything at zero for places where the mask has to go
+                        original_image = cv2.cvtColor(original_image, cv2.COLOR_RGB2BGR)
+                    #now lets draw on the original_image the left and right hand landmarks, the facemesh and the body poses
+                        #left hand
+                    if verbose:
                         mp_drawing.draw_landmarks(original_image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
                             #right hand
                         mp_drawing.draw_landmarks(original_image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
@@ -210,61 +209,67 @@ def cli_mp_main(args):
                                 mp_holistic.POSE_CONNECTIONS,
                                 landmark_drawing_spec=mp_drawing_styles.
                                 get_default_pose_landmarks_style())
-                        #######################now save everything to a time series
-                            #make a variable list with x, y, z, info where data is appended to
-                        samplebody = listpostions(results.pose_landmarks)
-                        samplehands = listpostions([results.left_hand_landmarks, results.right_hand_landmarks])
-                        sampleface = listpostions(results.face_landmarks)
-                        samplebody.insert(0, time)
-                        samplehands.insert(0, time)
-                        sampleface.insert(0, time)
-                        tsbody.append(samplebody)   #append to the timeseries object
-                        tshands.append(samplehands) #append to the timeseries object
-                        tsface.append(sampleface)   #append to the timeseries object
-                    #show the video as we process (you can comment this out, if you want to run this process in the background)
-                    if verbose:
-                        cv2.imshow("resizedimage", original_image)
-                    if save_video:
-                        out.write(original_image) #save the frame to the new masked video
-                    time = time+(1000/samplerate)#update the time variable  for the next frame
+                    #######################now save everything to a time series
+                        #make a variable list with x, y, z, info where data is appended to
+                    samplebody = listpostions(results.pose_landmarks)
+                    samplehands = listpostions([results.left_hand_landmarks, results.right_hand_landmarks])
+                    sampleface = listpostions(results.face_landmarks)
+                    samplebody.insert(0, time)
+                    samplehands.insert(0, time)
+                    sampleface.insert(0, time)
+                    tsbody.append(samplebody)   #append to the timeseries object
+                    tshands.append(samplehands) #append to the timeseries object
+                    tsface.append(sampleface)   #append to the timeseries object
+                #show the video as we process (you can comment this out, if you want to run this process in the background)
                 if verbose:
-                    if cv2.waitKey(1) == 27: #allow the use of ESCAPE to break the loop
-                        break
-                if ret == False: #if there are no more frames, break the loop
+                    cv2.imshow("resizedimage", original_image)
+                if save_video:
+                    out.write(original_image) #save the frame to the new masked video
+                time = time+(1000/samplerate)#update the time variable  for the next frame
+                print('frame {} done.'.format(nframe))
+            if verbose:
+                if cv2.waitKey(1) == 27: #allow the use of ESCAPE to break the loop
                     break
+            if ret == False: #if there are no more frames, break the loop
+                break
+            
 
-        #once done de-initialize all processes
-        if save_video:
-            out.release()
-        capture.release()
-        if verbose:
-            cv2.destroyAllWindows()
-        ####################################################### data to be written row-wise in csv fil
-        # opening the csv file in 'w+' mode
-        filebody = open(outputf_ts + vidf[:-4]+'_body.csv', 'w+', newline ='')
-        #write it
-        with filebody:    
-            write = csv.writer(filebody)
-            write.writerows(tsbody)
-        # opening the csv file in 'w+' mode
-        filehands = open(outputf_ts + vidf[:-4]+'_hands.csv', 'w+', newline ='')
-        #write it
-        with filehands:
-            write = csv.writer(filehands)
-            write.writerows(tshands)
-        # opening the csv file in 'w+' mode
-        fileface = open(outputf_ts + vidf[:-4]+'_face.csv', 'w+', newline ='')
-        #write it
-        with fileface:    
-            write = csv.writer(fileface)
-            write.writerows(tsface)
+    #once done de-initialize all processes
+    if save_video:
+        out.release()
+    capture.release()
+    if verbose:
+        cv2.destroyAllWindows()
+    ####################################################### data to be written row-wise in csv fil
+    # opening the csv file in 'w+' mode
+    filebody = Path(outputf_ts) / (videoname[:-4]+'_body.csv')
+    filebody = open(filebody, 'w+', newline ='')
+    #write it
+    with filebody:    
+        write = csv.writer(filebody)
+        write.writerows(tsbody)
+    # opening the csv file in 'w+' mode
+    filehands = Path(outputf_ts) / (videoname[:-4]+'_hands.csv')
+    filehands = open(filehands, 'w+', newline ='')
+    #write it
+    with filehands:
+        write = csv.writer(filehands)
+        write.writerows(tshands)
+    # opening the csv file in 'w+' mode
+    fileface = Path(outputf_ts) / (videoname[:-4]+'_face.csv')
+    fileface = open(fileface, 'w+', newline ='')
+    #write it
+    with fileface:    
+        write = csv.writer(fileface)
+        write.writerows(tsface)
 
-    print("Done with processing all folders; go look in your output folders!")    
+    print("Done with video")    
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('-mp', '--mypath', type=str, default='./videos')
+    # parser.add_argument('-mp', '--mypath', type=str, default='./videos')
+    parser.add_argument('-vf', '--vfile', type=str, required=True)
     # parser.add_argument('-if', '--ifolder', type=str, default='./videos')
     parser.add_argument('-of', '--outputf_ts', type=str, default='./video_outputs')
     parser.add_argument('--debug', action='store_true', default=False)
